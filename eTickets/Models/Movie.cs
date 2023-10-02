cbb38784e2bda1193f88cd0e2170ecdeba609146
Roshan <kroshan.shashendra@gmail.com>
2023-10-02 17:17:30 +0530

    2023 - added models and database connection

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using eTickets.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Price { get; set; }
+        public String ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //cinema
+        public int CinemaId { get; set; }
+        [ForeignKey ("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
+    }
+}
---
+++ eTickets/Startup.cs
@@ -0,0 +1,50 @@
+﻿using eTickets.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets
+{
+    public static class Startup
+    {
+        public static WebApplication initializeApp(string[] args) {
+
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
+            var app = builder.Build();
+            Confiugure(app);
+            return app;
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            //dbContext configuration
+            builder.Services.AddDbContext<AppDbContext>( options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionDString")));
+            builder.Services.AddControllersWithViews();
+
+        }
+
+        private static void Confiugure(WebApplication app)
+        {
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+        }
+    }
+}
