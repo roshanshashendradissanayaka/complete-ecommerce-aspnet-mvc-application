ed570eaaa6f7959844de3104e26efab39fb28081
Roshan <kroshan.shashendra@gmail.com>
2023-10-09 21:48:30 +0530

    Added new controllers and actors view

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        /*public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }*/
+
+        public async Task<IActionResult> Index()
+        {
+            var allActors = await _context.Actors.ToListAsync();
+            return View(allActors);
+        }
+    }
+}
